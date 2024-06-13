d4041df755be6195538555f4f4476b83d4a09e7e
erfan taheri <erfanta197@gmail.com>
2024-06-13 23:09:31 +0330

    implement remove method

--- src/Heaps.ts
+++ src/Heaps.ts
@@ -5,6 +5,32 @@ class Heaps {
     this.items.push(value);
     this.bubbleUp();
   }
+  public remove() {
+    let index = 0;
+    this.items[index] = this.items[this.items.length - 1];
+    this.items.pop();
+
+    while (index < this.items.length) {
+      let leftIndex = index * 2 + 1;
+      let rightIndex = index * 2 + 2;
+      let largerIndex = 0;
+
+      if (
+        rightIndex < this.items.length &&
+        this.items[rightIndex] > this.items[leftIndex]
+      ) {
+        largerIndex = rightIndex;
+      } else if (leftIndex < this.items.length) {
+        largerIndex = leftIndex;
+      }
+      if (largerIndex === 0 || this.items[index] >= this.items[largerIndex]) {
+        break;
+      }
+
+      this.swap(index, largerIndex);
+      index = largerIndex;
+    }
+  }
 
   private bubbleUp() {
     let index: number = this.items.length - 1;
