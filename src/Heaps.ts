8cc1ce2a44388cb3aa6f45e5bfee335357c7ffcf
erfan taheri <erfanta197@gmail.com>
2024-06-13 23:05:17 +0330

    refactoring making swap method more reusable

--- src/Heaps.ts
+++ src/Heaps.ts
@@ -1,28 +1,30 @@
 class Heaps {
-  private items: number[] = [];
+  items: number[] = [];
 
   public insert(value: number) {
     this.items.push(value);
-    this.bubleUp();
+    this.bubbleUp();
   }
 
-  private bubleUp() {
+  private bubbleUp() {
     let index: number = this.items.length - 1;
     while (
       index > 0 &&
       this.items[index] > this.items[this.parentIndex(index)]
     ) {
-      this.swap(index);
+      this.swap(index, this.parentIndex(index));
       index = this.parentIndex(index);
     }
   }
   private parentIndex(index: number) {
     return Math.floor((index - 1) / 2);
   }
-  private swap(index: number) {
-    let temp = this.items[index];
-    this.items[index] = this.items[this.parentIndex(index)];
-    this.items[this.parentIndex(index)] = temp;
+  private swap(index1: number, index2: number) {
+    if (this.items[index1] !== this.items[index2]) {
+      let temp = this.items[index1];
+      this.items[index1] = this.items[index2];
+      this.items[index2] = temp;
+    }
   }
 }
 
