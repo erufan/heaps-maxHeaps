e0978526b3380cddf3e99524ece697a9fce5f2ed
erfan taheri <erfanta197@gmail.com>
2024-06-13 13:49:48 +0330

    implement insert method

--- src/Heaps.ts
+++ src/Heaps.ts
@@ -1,3 +1,29 @@
-class Heaps {}
+class Heaps {
+  private items: number[] = [];
+
+  public insert(value: number) {
+    this.items.push(value);
+    this.bubleUp();
+  }
+
+  private bubleUp() {
+    let index: number = this.items.length - 1;
+    while (
+      index > 0 &&
+      this.items[index] > this.items[this.parentIndex(index)]
+    ) {
+      this.swap(index);
+      index = this.parentIndex(index);
+    }
+  }
+  private parentIndex(index: number) {
+    return Math.floor((index - 1) / 2);
+  }
+  private swap(index: number) {
+    let temp = this.items[index];
+    this.items[index] = this.items[this.parentIndex(index)];
+    this.items[this.parentIndex(index)] = temp;
+  }
+}
 
 export default Heaps;
--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,13 @@ import Heaps from "./Heaps";
 
 const heap = new Heaps();
 
+heap.insert(2);
+heap.insert(23);
+heap.insert(9);
+heap.insert(12);
+heap.insert(23);
+heap.insert(32);
+heap.insert(421);
+heap.insert(45);
+
 console.log(heap);
